910a352b578ecb9290d3ca44746cce47b696c952
ABHISHEK RAI <abhi_rai10@rediffmail.com>
2024-05-01 18:40:04 +0530

    Refactored code

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,29 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using DataStructure.Tree.BinaryTree;
 
-var node = Node.CreateBinaryTree();
-Console.WriteLine($"\nInOrder Traversal");
-InOrderTraverse.Traverse(node);
-
-Console.WriteLine("\nIterative InOrder Traversal");
-IterativeInOrderTraverse.Traverse(node);
-
-Console.WriteLine("\nPreOrder Traversal");
-PreOrderTraverse.Traverse(node);
-
-Console.WriteLine("\nIterative PreOrder Traversal");
-IterativePreOrderTraverse.Traverse(node);
-
-Console.WriteLine("\nIterative PreOrder Space Optimized Traversal");
-IterativePreOrderSpaceOptimizedTraverse.Traverse(node);
-
-Console.WriteLine("\nPostOrder Traversal");
-PostOrderTraverse.Traverse(node);
-
-Console.WriteLine($"\nSizeOfBinaryTree - {SizeOfBinaryTree.Get(node)}");
-
-Console.WriteLine($"Max Value in Binary Tree - {MaxValueInBinaryTree.Get(node)}");
-
-Console.WriteLine($"Max Value in Binary Tree - {HeightOfBinaryTree.Get(node)}");
+DataStructure.Tree.BinaryTree.Helper.ExecutePrograms();
 
 
---
+++ DataStructure/Tree/BinaryTree/Helper.cs
@@ -0,0 +1,35 @@
+﻿namespace DataStructure.Tree.BinaryTree
+{
+    public class Helper
+    {
+
+        public static void ExecutePrograms()
+        {
+            var node = Node.CreateBinaryTree();
+            Console.WriteLine($"\nInOrder Traversal");
+            InOrderTraverse.Traverse(node);
+
+            Console.WriteLine("\nIterative InOrder Traversal");
+            IterativeInOrderTraverse.Traverse(node);
+
+            Console.WriteLine("\nPreOrder Traversal");
+            PreOrderTraverse.Traverse(node);
+
+            Console.WriteLine("\nIterative PreOrder Traversal");
+            IterativePreOrderTraverse.Traverse(node);
+
+            Console.WriteLine("\nIterative PreOrder Space Optimized Traversal");
+            IterativePreOrderSpaceOptimizedTraverse.Traverse(node);
+
+            Console.WriteLine("\nPostOrder Traversal");
+            PostOrderTraverse.Traverse(node);
+
+            Console.WriteLine($"\nSizeOfBinaryTree - {SizeOfBinaryTree.Get(node)}");
+
+            Console.WriteLine($"Max Value in Binary Tree - {MaxValueInBinaryTree.Get(node)}");
+
+            Console.WriteLine($"Max Value in Binary Tree - {HeightOfBinaryTree.Get(node)}");
+        }
+
+    }
+}
